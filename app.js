8aeded8df5be9862a0456fd1469b70c8977b5915
Henry <henry.onyango@outlook.com>
2018-09-26 07:43:47 +0300

    - Implement morgan in app.js for logging response out

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const morgan = require('morgan');
+
 // Routes
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+// Logging using morgan
+app.use(morgan('dev'))
 
 // Middleware to handle requests: Define API end points
 app.use('/products', productRoutes);
