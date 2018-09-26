ccd4b80833f59e2346bd78f06c0f6936a4a003d3
Henry <henry.onyango@outlook.com>
2018-09-26 07:52:19 +0300

    - Handle 404 errors - Handle errors that would be thrown by failures in the system

--- app.js
+++ app.js
@@ -16,4 +16,21 @@ app.use('/products', productRoutes);
 
 app.use('/orders', ordersRoutes)
 
+// Non-existing routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// Handling errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 module.exports = app;
