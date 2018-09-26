9d0c0c45323b9113be59d1cdb278b18fe7750c87
Henry <henry.onyango@outlook.com>
2018-09-26 08:12:48 +0300

    - Set cors headers

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// CORS headers
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, AUthorization');
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({})
+    }
+})
+
+
 // Middleware to handle requests: Define API end points
 app.use('/products', productRoutes);
 
